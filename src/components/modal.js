f23f1705c011c832daeb243fdf1dd69fc4b32bd9
Adelina Eryslanova <ravilevna.adelina@yandex.ru>
2025-02-05 19:12:25 +0500

    Добавление открытия и закрития кнопки редоктиров-е

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,8 +1,11 @@
 // Находим форму в DOM
-const formElement = document.querySelector(".popup"); // Воспользуйтесь методом querySelector()
+
+import { handelCloseButton, handelEscButton, handelOverlayClick } from "..";
+
+const formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-const nameInput = document.querySelector(".popup"); // Воспользуйтесь инструментом .querySelector()
-const jobInput = document.querySelector(".popup"); // Воспользуйтесь инструментом .querySelector()
+const nameInput = formElement.querySelector(".popup__input_type_name"); // Воспользуйтесь инструментом .querySelector()
+const jobInput = formElement.querySelector(".popup__input_type_description"); // Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -13,11 +16,37 @@ function handleFormSubmit(evt) {
 
   // Получите значение полей jobInput и nameInput из свойства value
 
+  const name = nameInput.value;
+  const job = jobInput.value;
+
   // Выберите элементы, куда должны быть вставлены значения полей
 
+  const profileTitle = document.querySelector(".profile__title");
+
+  const profileDescription = document.querySelector(".profile__description");
+
   // Вставьте новые значения с помощью textContent
+
+  profileTitle.textContent = name;
+  profileDescription.textContent = job;
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", handleFormSubmit);
+
+export function openModal(modalObj) {
+  const modalCloseButton = modalObj.querySelector(".popup__close");
+
+  modalObj.style.display = "flex";
+  modalObj.classList.add("modal");
+
+  modalCloseButton.addEventListener("click", handelCloseButton);
+  document.addEventListener("keydown", handelEscButton);
+  modalObj.addEventListener("click", handelOverlayClick);
+}
+
+export function closeModal(modalObj) {
+  modalObj.style.display = "none";
+  modalObj.classList.remove("modal");
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./pages/index.css";
 import { initialCards } from "./scripts/cards";
-import { createCard, handleDeleteCard, handleLikeCard, handleImageClick} from "./components/card";
+import {
+  createCard,
+  handleDeleteCard,
+  handleLikeCard,
+} from "./components/card";
+import { closeModal, openModal } from "./components/modal";
 
 // @todo: Темплейт карточки
 
@@ -10,17 +15,83 @@ export const cardTemplate = document.querySelector("#card-template").content;
 
 const container = document.querySelector(".places__list");
 
-
-
 // @todo: Вывести карточки на страницу
 
 function conclusionСard(cardList) {
-  
   cardList.forEach((card) => {
-    container.append(createCard(card, handleDeleteCard, handleLikeCard, handleImageClick));
+    container.append(
+      createCard(card, handleDeleteCard, handleLikeCard, handleImageClick)
+    );
   });
 }
 
+function handleImageClick(evt) {
+  //получаем обьект popup
 
+  const popup = document.querySelector(".popup_type_image");
+  const popupImage = popup.querySelector(".popup__image");
+  const popupCaption = popup.querySelector(".popup__caption");
+
+  //устанавливаем popup картинку дочернего элемента
+
+  popupImage.src = evt.target.src;
+  popupCaption.textContent = evt.target.alt;
+
+  openModal(popup);
+}
+
+const popupEditButton = document.querySelector(".profile__edit-button");
+
+function handleEditButton(evt) {
+  const popup = document.querySelector(".popup_type_edit");
+
+  openModal(popup);
+}
+
+popupEditButton.addEventListener("click", handleEditButton);
+
+const popupAddButton = document.querySelector(".profile__add-button");
+
+function handleAddButton(evt) {
+  const popup = document.querySelector(".popup_type_new-card");
+
+  openModal(popup);
+}
+
+popupAddButton.addEventListener("click", handleAddButton);
+
+export function handelCloseButton(evt) {
+  const popupToClose = evt.target.closest(".popup");
+
+  closeModal(popupToClose);
+
+  popupToClose.removeEventListener("click", handelCloseButton);
+  document.removeEventListener("keydown", handelEscButton);
+  popupToClose.removeEventListener("click", handelOverlayClick);
+}
+
+export function handelEscButton(evt) {
+  const popupToClose = document.querySelector(".modal");
+
+  if (evt.key === "Escape") {
+    closeModal(popupToClose);
+
+    popupToClose.removeEventListener("click", handelCloseButton);
+    document.removeEventListener("keydown", handelEscButton);
+    popupToClose.removeEventListener("click", handelOverlayClick);
+  }
+}
+
+export function handelOverlayClick(evt) {
+  const popupToClose = evt.target.closest(".popup");
+
+  if (evt.target === evt.currentTarget) {
+    closeModal(popupToClose);
+
+    popupToClose.removeEventListener("click", handelCloseButton);
+    document.removeEventListener("keydown", handelEscButton);
+    popupToClose.removeEventListener("click", handelOverlayClick);
+  }
+}
 
 conclusionСard(initialCards);
