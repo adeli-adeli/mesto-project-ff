80e41171f070e66a5ec316df2e2dc760e7479db2
Adelina Eryslanova <ravilevna.adelina@yandex.ru>
2025-02-11 19:26:52 +0500

    иправила ошибки вынесла все const и добавила атрибут alt в функции

--- src/index.js
+++ src/index.js
@@ -15,9 +15,13 @@ export const cardTemplate = document.querySelector("#card-template").content;
 
 const container = document.querySelector(".places__list");
 
+const popupTypeImage = document.querySelector(".popup_type_image");
+const popupImage = popupTypeImage.querySelector(".popup__image");
+const popupCaption = popupTypeImage.querySelector(".popup__caption");
+
 // @todo: Вывести карточки на страницу
 
-function conclusionСard(cardList) {
+function conclusionCard(cardList) {
   cardList.forEach((card) => {
     container.append(
       createCard(card, handleDeleteCard, handleLikeCard, handleImageClick)
@@ -28,24 +32,20 @@ function conclusionСard(cardList) {
 // открытие popup с картинкой
 
 function handleImageClick(evt) {
-  const popup = document.querySelector(".popup_type_image");
-  const popupImage = popup.querySelector(".popup__image");
-  const popupCaption = popup.querySelector(".popup__caption");
-
   popupImage.src = evt.target.src;
+  popupImage.alt = evt.target.alt;
   popupCaption.textContent = evt.target.alt;
 
-  openModal(popup);
+  openModal(popupTypeImage);
 }
 
 // открытия профиля
 
 const popupEditButton = document.querySelector(".profile__edit-button");
+const popupTypeEdit = document.querySelector(".popup_type_edit");
 
 function handleEditButton(evt) {
-  const popup = document.querySelector(".popup_type_edit");
-
-  openModal(popup);
+  openModal(popupTypeEdit);
 }
 
 popupEditButton.addEventListener("click", handleEditButton);
@@ -53,11 +53,10 @@ popupEditButton.addEventListener("click", handleEditButton);
 // открытия добавления карточки на +
 
 const popupAddButton = document.querySelector(".profile__add-button");
+const popupTypeNewCard = document.querySelector(".popup_type_new-card");
 
 function handleAddButton(evt) {
-  const popup = document.querySelector(".popup_type_new-card");
-
-  openModal(popup);
+  openModal(popupTypeNewCard);
 }
 
 popupAddButton.addEventListener("click", handleAddButton);
@@ -77,9 +76,9 @@ export function handleCloseButton(evt) {
 // закрытие popup на кнопку
 
 export function handleEscButton(evt) {
-  const popupToClose = document.querySelector(".popup_is-opened");
-
   if (evt.key === "Escape") {
+    const popupToClose = document.querySelector(".popup_is-opened");
+
     closeModal(popupToClose);
 
     popupToClose.removeEventListener("click", handleCloseButton);
@@ -102,4 +101,4 @@ export function handleOverlayClick(evt) {
   }
 }
 
-conclusionСard(initialCards);
+conclusionCard(initialCards);
